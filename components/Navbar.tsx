0652844b4e955822651fbe77346c3bc7d6fe662a
davidsdosen <david.sorhaug.dosen@gmail.com>
2026-03-02 22:24:07 +0100

    simple Navbar.tsx created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <header>
+            <nav className="flex justify-between items-center py-5 px-10">
+                <Link href="/" className="logo">
+                    <Image src={"/icons/logo.png"} alt={"logo"} width={24} height={24}/>
+
+                    <p>DevEvent</p>
+                </Link>
+
+                <ul className="flex gap-10">
+                    <li><Link href="#/">Home</Link></li>
+                    <li><Link href="/">Events</Link></li>
+                    <li><Link href="/">Create Event</Link></li>
+                </ul>
+            </nav>
+        </header>
+
     )
 }
 export default Navbar
