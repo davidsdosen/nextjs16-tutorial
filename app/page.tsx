83cdc70be7d08d66754cbf238c6e54678912bf0a
davidsdosen <david.sorhaug.dosen@gmail.com>
2026-03-02 06:47:46 +0100

    fixed false errors and made changes suggested by IDE to make code best practice

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,17 @@ const Page = () => {
             <p className={"text-center mt-10"}>Hackathons, Meetups, and Conferences, All in One Place</p>
         <ExploreBtn/>
 
+        <div className={"mt-20 space-y-7"}>
+            <h3>Featured events</h3>
+            <ul className={"events"}>
+                {[1,2,3,4,5,6].map((event) => (
+                    <li key={event}>Event {event}</li>
+                ))}
+            </ul>
+        </div>
+
 
         </section>
-
-
-
     )
 }
 export default Page
