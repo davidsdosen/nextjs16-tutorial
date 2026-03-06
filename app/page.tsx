8e5d5c8fa9668c49faf576eca24e7ad873d10de6
davidsdosen <david.sorhaug.dosen@gmail.com>
2026-03-06 02:04:30 +0100

    added simple Event Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import ExploreBtn from "@/components/ExploreBtn";
 
+const events = [
+    { image: '/images/event-1.png', title: 'Event 1' },
+    { image: '/images/event-2.png', title: 'Event 2' },
+]
+
 const Page = () => {
     return (
         <section>
@@ -9,7 +14,7 @@ const Page = () => {
 
         <div className={"mt-20 space-y-7"}>
             <h3>Featured events</h3>
-            <ul className={"events"}>
+            <ul className={"events list-none"}>
                 {[1,2,3,4,5,6].map((event) => (
                     <li key={event}>Event {event}</li>
                 ))}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,12 +7,11 @@ const Navbar = () => {
             <nav className="flex justify-between items-center py-5 px-10">
                 <Link href="/" className="logo">
                     <Image src={"/icons/logo.png"} alt={"logo"} width={24} height={24}/>
-
                     <p>DevEvent</p>
                 </Link>
 
-                <ul className="flex gap-10">
-                    <li><Link href="#/">Home</Link></li>
+                <ul className="flex gap-10 list-none">
+                    <li><Link href="/">Home</Link></li>
                     <li><Link href="/">Events</Link></li>
                     <li><Link href="/">Create Event</Link></li>
                 </ul>
