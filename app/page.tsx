59eeae1adccbaf9871d43ee6774a7181d49b3b81
davidsdosen <david.sorhaug.dosen@gmail.com>
2026-03-06 04:33:36 +0100

    applied EsLint recommendation: Error:(8, 90) ESLint: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`. (react/no-unescaped-entities)

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import {events} from "@/lib/constants";
 const Page = () => {
     return (
         <section>
-            <h1 className="text-center font-bold">The Hub for every Dev Event <br /> you can't miss!</h1>
+            <h1 className="text-center font-bold">The Hub for every Dev Event <br /> you can&#39;t miss!</h1>
             <p className={"text-center mt-10"}>Hackathons, Meetups, and Conferences, All in One Place</p>
         <ExploreBtn/>
 
